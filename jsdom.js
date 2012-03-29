362deac083e21e291c532957c0264723bde41749
Caesar Chi <caesar_chi@miiicasa.com>
2012-03-29 18:12:13 +0800

    update jsdom fetch many photos.

--- jsdom.js
+++ jsdom.js
@@ -1,28 +1,29 @@
 var jsdom  = require('jsdom');
 var fs     = require('fs');
 var jquery = fs.readFileSync("./js/jquery.js").toString();
-var obj = [];
+var sourceUrl = [
+        'http://www.wretch.cc/blog/wretchbeauty/12760490',
+        'http://www.wretch.cc/blog/wretchbeauty/12760527',
+        'http://www.wretch.cc/blog/wretchbeauty/12760512'
+    ];
 
-jsdom.env({
-  html: 'http://www.wretch.cc/blog/wretchbeauty/12760542',
-  src: [
-    jquery
-  ],
-  done: function(errors, window) {
-    var $ = window.$;
-    $(".innertext img").each(function (idx, el) {
-        var nodeSrc = $(el).attr("src");
-        console.log(nodeSrc);
-        obj.push(nodeSrc);
+function fetchImg (url) {
+    jsdom.env({
+      html: url,
+      src: [
+        jquery
+      ],
+      done: function(errors, window) {
+        var $ = window.$;
+        $(".innertext img").each(function (idx, el) {
+            var nodeSrc = $(el).attr("src");
+            console.log(nodeSrc);
+        });
+      }
     });
-  }
+}
+
+sourceUrl.forEach(function (el, idx) {
+    fetchImg(el);
 });
 
-var http = require('http');
-
-var server = http.createServer(function(req, res){
-     // your normal server code
-     res.writeHead(200, {'Content-Type': 'application/json'});
-     res.end(JSON.stringify(obj));
-});
-server.listen(8787);
