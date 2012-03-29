00bb70079851f2350276446f10b958571d9246f5
Caesar Chi <caesar_chi@miiicasa.com>
2012-03-29 18:00:19 +0800

    server can show simple api.

--- jsdom.js
+++ jsdom.js
@@ -1,32 +1,28 @@
-var req,
-    http = require('http'),
-    jsdom = require('jsdom'),
-    htmlContent = '',
-    options = {
-      host: 'tw.yahoo.com',
-      port: 80,
-      path: '/index.html',
-      method: 'POST'
-    };
+var jsdom  = require('jsdom');
+var fs     = require('fs');
+var jquery = fs.readFileSync("./js/jquery.js").toString();
+var obj = [];
 
-console.log(htmlContent);
-
-// Get service is working
-req = http.request(options, function(res) {
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-        htmlContent += chunk;
+jsdom.env({
+  html: 'http://www.wretch.cc/blog/wretchbeauty/12760542',
+  src: [
+    jquery
+  ],
+  done: function(errors, window) {
+    var $ = window.$;
+    $(".innertext img").each(function (idx, el) {
+        var nodeSrc = $(el).attr("src");
+        console.log(nodeSrc);
+        obj.push(nodeSrc);
     });
-    res.on('end', function (chunk) {
-        console.log(htmlContent);
-        console.log('data is end\n');
-    });
+  }
 });
 
-req.on('error', function(e) {
-    console.log("Got error: " + e.message);
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+     // your normal server code
+     res.writeHead(200, {'Content-Type': 'application/json'});
+     res.end(JSON.stringify(obj));
 });
-
-// notice have to add req end;
-req.end();
-
+server.listen(8787);
