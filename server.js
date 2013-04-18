c1c61c17cc7aedb51ca03132e06f0f0e29dac56d
clonn <clonncd@gmail.com>
2013-04-18 11:47:07 +0800

    update server for demo perpose

--- server.js
+++ server.js
@@ -1,36 +1,6 @@
-var express = require('express')
-  , app = express.createServer();
-
-var users = [{ name: 'tj' }];
-
-app.all('/user/:id/:op?', function(req, res, next){
-  req.user = users[req.params.id];
-  if (req.user) {
-    console.log("what is next");
-    next();
-  } else {
-    next(new Error('cannot find user ' + req.params.id));
-  }
-});
-
-app.get('/user/:id', function(req, res){
-  console.log("get ID is next");
-  res.send('viewing ' + req.user.name);
-});
-
-app.get('/user/:id/edit', function(req, res){
-  console.log("edit ID is next");
-  res.send('editing ' + req.user.name);
-});
-
-app.put('/user/:id', function(req, res){
-  console.log("put ID is next");
-  res.send('updating ' + req.user.name);
-});
-
-app.get('*', function(req, res){
-  console.log(" * ID is next");
-  res.send('what???', 404);
-});
-
-app.listen(3000);
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
